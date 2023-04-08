8e51013c594fbb7b9fe627b39d97aa6378a019ce
HyeonJun Kang <khjj9865@naver.com>
2023-04-08 13:51:31 +0900

    initial commit

---
+++ ExchangeRateCalculator/View/InterestTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  InterestTableViewCell.swift
+//  ExchangeRateCalculator
+//
+//  Created by 강현준 on 2023/04/08.
+//
+
+import UIKit
+
+class InterestTableViewCell: UITableViewCell {
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
