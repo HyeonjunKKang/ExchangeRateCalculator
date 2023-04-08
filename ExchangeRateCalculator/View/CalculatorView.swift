191ec8bcdbebaee9804233f8f3fea4fb11e0b230
HyeonJun Kang <khjj9865@naver.com>
2023-04-08 14:58:14 +0900

    initial commit

---
+++ ExchangeRateCalculator/View/CalculatorView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorView.swift
+//  ExchangeRateCalculator
+//
+//  Created by 강현준 on 2023/04/08.
+//
+
+import UIKit
+
+class CalculatorView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
